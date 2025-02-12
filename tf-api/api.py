178a8abe5e9522cea73705769944f1e85b0e9e8c
Arik Hadas <ahadas@redhat.com>
2025-02-12 15:22:20 +0200

    improve logging of exporters

--- tf-api/api.py
+++ tf-api/api.py
@@ -231,7 +231,7 @@ def get_boards(board_type):
         exporter['borrowed'] = False
         return exporter
 
-    logging.info(f"exporters: {exporters}")
+    logging.info(f"exporters:\n{json.dumps(exporters, indent=2)}")
     return list(map(to_board, exporters['items']))
 
 def get_run_name(run_id):
